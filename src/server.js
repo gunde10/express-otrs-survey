c525a53898c1c59aaee19d9b07878188fc5e6d42
Rickard Jarnling <rickard.jarnling@on1call.se>
2023-02-15 22:00:06 +0100

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,37 +1,22 @@
-/**
- * The starting point of the application.
- *
- * @author Rickard Jarnling
- * @version 1.0.0
- */
-
 import express from 'express'
-import hbs from 'express-hbs'
-import session from 'express-session'
 import logger from 'morgan'
 import helmet from 'helmet'
+import path from 'path'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-import { router } from './routes/router.js'
-import { connectDB } from './config/mongoose.js'
 
 /**
  * The main function of the application.
  */
 const main = async () => {
-  await connectDB()
-
   const app = express()
   const directoryFullName = dirname(fileURLToPath(import.meta.url))
-
-  const baseURL = process.env.BASE_URL || '/'
 
   // Set up a morgan logger using the dev format for log entries.
   app.use(logger('dev'))
 
   // Use helmet for security purposes
   // Set various HTTP headers to make the application little more secure (https://www.npmjs.com/package/helmet).
-  // (The web application uses external scripts and therefore needs to explicitly trust on code.jquery.com and cdn.jsdelivr.net.)
   app.use(helmet())
   app.use(
     helmet.contentSecurityPolicy({
@@ -42,93 +27,18 @@ const main = async () => {
     })
   )
 
-  // View engine setup.
-  app.engine('hbs', hbs.express4({
-    defaultLayout: join(directoryFullName, 'views', 'layouts', 'default'),
-    partialsDir: join(directoryFullName, 'views', 'partials')
-  }))
-  app.set('view engine', 'hbs')
-  app.set('views', join(directoryFullName, 'views'))
-
   // Parse requests of the content type application/x-www-form-urlencoded.
   // Populates the request object with a body object (req.body).
   app.use(express.urlencoded({ extended: false }))
   app.use(express.json())
 
+  // Serve views/index.html
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(directoryFullName, "views/index.html"))
+  })
+
   // Serve static files.
   app.use(express.static(join(directoryFullName, '..', 'public')))
-
-  // Setup and use session middleware (https://github.com/expressjs/session)
-  const sessionOptions = {
-    name: process.env.SESSION_NAME, // Don't use default session cookie name.
-    secret: process.env.SESSION_SECRET, // Change it!!! The secret is used to hash the session with HMAC.
-    resave: false, // Resave even if a request is not changing the session.
-    saveUninitialized: false, // Don't save a created but not modified session.
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-      sameSite: 'lax'
-    }
-  }
-
-  if (app.get('env') === 'production') {
-    app.set('trust proxy', 1)
-    sessionOptions.cookie.secure = true
-  }
-
-  app.use(session(sessionOptions))
-
-  // Middleware to be executed before the routes.
-  app.use((req, res, next) => {
-    // Flash messages - survives only a round trip.
-    if (req.session.flash) {
-      res.locals.flash = req.session.flash
-      delete req.session.flash
-    }
-
-    // To change the header/navbar when a user logs in or out.
-    if (req.session.loggedIn) {
-      res.locals.loggedIn = req.session.loggedIn
-      res.locals.username = req.session.username
-    }
-
-    // Pass the base URL to the views.
-    res.locals.baseURL = baseURL
-
-    next()
-  })
-  // Register routes.
-  app.use('/', router)
-
-  // Error handler.
-  app.use(function (err, req, res, next) {
-    if (err.status === 401) {
-      return res
-        .status(401)
-    }
-
-    if (err.status === 403) {
-      return res
-        .status(403)
-        .sendFile(join(directoryFullName, 'views', 'errors', '403.html'))
-    }
-
-    if (err.status === 404) {
-      return res
-        .status(404)
-        .sendFile(join(directoryFullName, 'views', 'errors', '404.html'))
-    }
-
-    if (req.app.get('env') !== 'development') {
-      return res
-        .status(500)
-        .sendFile(join(directoryFullName, 'views', 'errors', '500.html'))
-    }
-
-    res
-      .status(err.status || 500)
-      .render('errors/error', { error: err })
-  })
 
   // Starts the HTTP server listening for connections.
   app.listen(process.env.PORT, () => {
